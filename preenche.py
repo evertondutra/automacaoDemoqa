3ce36e9d652714f045ac16218509f8dda019df55
EvertonDutra <evertondutra02@gmail.com>
2021-06-02 05:37:13 -0300

    criando uma classe Preenche elementos com object

--- preenche.py
+++ preenche.py
@@ -1,19 +1,27 @@
 from browser import Browser
 
 
+class PreencheElementos(object):
+    idName = 'userName'
+    idEmail = 'userEmail'
+    idAddress = 'currentAddress'
+    IdPermanentAddress = 'permanentAddress'
+    botomSubmit = 'submit'
+
+
 class Preenche(Browser):
     def name(self, name):
-        self.driver.find_element_by_id('userName').send_keys(name)
+        self.driver.find_element_by_id(PreencheElementos.idName).send_keys(name)
 
     def email(self, email):
-        self.driver.find_element_by_id('userEmail').send_keys(email)
+        self.driver.find_element_by_id(PreencheElementos.idEmail).send_keys(email)
 
     def address(self, address):
-        self.driver.find_element_by_id('currentAddress').send_keys(address)
+        self.driver.find_element_by_id(PreencheElementos.idAddress).send_keys(address)
 
     def perAddress(self, perAddress):
-        self.driver.find_element_by_id('permanentAddress').send_keys(perAddress)
+        self.driver.find_element_by_id(PreencheElementos.IdPermanentAddress).send_keys(perAddress)
 
     # clicar no botão submit
     def cl_btn(self):
-        self.driver.find_element_by_id('submit').click()
+        self.driver.find_element_by_id(PreencheElementos.botomSubmit).click()
