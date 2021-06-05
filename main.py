c4b0cfd8aaed6b748110d49a7803f502ab988fa2
EvertonDutra <evertondutra02@gmail.com>
2021-06-05 03:22:59 -0300

    criado a 4 ação para conferir o resultado

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from utils import Utils
 from preenche import Preenche
+from results import Results
+from nose.tools import assert_true
 
 
 # Estanciando a classe
 utils = Utils()
 preenche = Preenche()
+results = Results()
 
 
 # 1ª ação: navegar até o site para automação
@@ -12,7 +15,7 @@ utils.navegar('https://demoqa.com/text-box')
 
 
 # 2ª ação: Preencher os campos
-preenche.name('Teste Teste')
+#preenche.name('Teste Teste')
 preenche.email('teste.teste@teste.com')
 preenche.address('Cidade Teste')
 preenche.perAddress('Cidade TesteTeste')
@@ -21,3 +24,5 @@ preenche.perAddress('Cidade TesteTeste')
 # 3ª ação: Clicar no botão Submit
 preenche.cl_btn()
 
+# 4ª acão: verificar resultado
+assert_true(results.get_text(), 'Nenhum campo foi preenchido')
